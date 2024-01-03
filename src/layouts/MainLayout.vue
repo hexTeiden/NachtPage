82bfe07f01309152000eae436915bd31a3741dd1
Hex Teiden <knogler.m19@htlwienwest.at>
2024-01-04 00:34:55 +0100

    Added all the files that have been existing to the repository

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,98 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar class="bg-dark">
+        <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
+        <q-toolbar-title> Nachtkastel Webpage </q-toolbar-title>
+
+        <div>Host: Knogler Matteo</div>
+
+        <q-btn flat dense round icon="menu">
+          <q-menu>
+            <q-list style="min-width: 100px">
+              <q-item clickable>
+                <q-item-section>Nachtkastel</q-item-section>
+              </q-item>
+              <q-item clickable>
+                <q-item-section>Projektteam</q-item-section>
+              </q-item>
+              <q-item clickable>
+                <q-item-section>3D Modell</q-item-section>
+              </q-item>
+            </q-list>
+          </q-menu>
+        </q-btn>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered class="bg-dark text-white">
+      <q-list class="text-white">
+        <q-item-label header> Important Links </q-item-label>
+
+        <EssentialLink v-for="link in essentialLinks" :key="link.title" v-bind="link" />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue';
+import EssentialLink from 'components/EssentialLink.vue';
+
+const linksList = [
+  {
+    title: 'School',
+    icon: 'school',
+    link: 'https://www.htlwienwest.at',
+  },
+  {
+    title: 'Github',
+    icon: 'code',
+    link: 'https://github.com/hexTeiden',
+  },
+  {
+    title: 'Google Drive',
+    icon: 'code',
+    link: 'https://drive.google.com/drive/u/1/folders/1yhL-wdOVhkSTPcLWzdt-2e4JwiBkqxiV',
+  },
+  {
+    title: 'Vue Framework',
+    icon: 'public',
+    link: 'https://vuejs.org',
+  },
+  {
+    title: 'Quasar Framework',
+    icon: 'public',
+    link: 'https://quasar.dev',
+  },
+  {
+    title: 'Used Font',
+    icon: 'chat',
+    link: 'https://fonts.google.com/specimen/Source+Code+Prodev',
+  },
+];
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    EssentialLink,
+  },
+
+  setup() {
+    const leftDrawerOpen = ref(false);
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+  },
+});
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,74 @@
+<template>
+  <q-page class="flex flex-center bg-blue-grey-9">
+    <q-layout view="lHh Lpr lFf" class="shadow-2 rounded-borders">
+      <div class="q-pa-md q-gutter-sm">
+        <q-banner
+          rounded
+          :class="$q.dark.isActive ? 'bg-grey-9' : 'bg-grey-2'"
+          style="width: 700px"
+        >
+          <template v-slot:avatar>
+            <img src="./images/3D_Modell.png" style="width: 100px; height: 64px" />
+          </template>
+          <div class="text-h5">Willkommen zum Projekt "Nachtkastel"</div>
+          <div>
+            Das Projekt "Nachtkastel" ist ein Nachtkasten welcher aus gebrauchten Materialien
+            zusammengebaut wurde. <br />
+          </div>
+        </q-banner>
+      </div>
+      <div class="q-pa-md row items-start q-gutter-md">
+        <q-card class="my-card">
+          <img src="./images/Matteo.png" />
+
+          <q-card-section>
+            <div class="text-h6">Matteo Knogler</div>
+            <div class="text-subtitle2">Rolle: Projektleiter</div>
+          </q-card-section>
+
+          <q-card-section class="q-pt-none">
+            {{ lorem }}
+          </q-card-section>
+        </q-card>
+        <q-card class="my-card">
+          <img src="./images/Matteo.png" />
+
+          <q-card-section>
+            <div class="text-h6">Lukas Ivejic</div>
+            <div class="text-subtitle2">Rolle: Projektmitglied</div>
+          </q-card-section>
+
+          <q-card-section class="q-pt-none">
+            {{ lorem }}
+          </q-card-section>
+        </q-card>
+        <q-card class="my-card">
+          <img src="./images/Matteo.png" />
+
+          <q-card-section>
+            <div class="text-h6">Sayed Sadat</div>
+            <div class="text-subtitle2">Rolle: Projektmitglied</div>
+          </q-card-section>
+
+          <q-card-section class="q-pt-none">
+            {{ lorem }}
+          </q-card-section>
+        </q-card>
+      </div>
+    </q-layout>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'IndexPage',
+});
+</script>
+
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 250px
+</style>
