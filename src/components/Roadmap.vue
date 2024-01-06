2d5023ac0d7a8fadd647e30fab0137f116801d04
Hex Teiden <knogler.m19@htlwienwest.at>
2024-01-06 21:31:37 +0100

    Changed the design of the roadmap a bit

--- src/components/Roadmap.vue
+++ src/components/Roadmap.vue
@@ -26,45 +26,6 @@
         <div>Projekt wurde beendet und die Technische Abnahme wurde durchgeführt.</div>
       </q-timeline-entry>
 
-      <!-- Website Part der Roadmap -->
-
-      <q-timeline-entry heading>Website</q-timeline-entry>
-
-      <q-timeline-entry title="Wireframe" subtitle="24 November 2023" side="left" icon="done_all">
-        <div>Wireframe wurde erstellt und die Struktur der Website wurde festgelegt.</div>
-      </q-timeline-entry>
-
-      <q-timeline-entry title="Mockup" subtitle="27 November 2023" side="right" icon="done_all">
-        <div>
-          Mockup wurde erstellt und die Farben und das Design der Website wurden festgelegt.
-        </div>
-      </q-timeline-entry>
-
-      <q-timeline-entry
-        title="Beginn der Website"
-        subtitle="1 Dezember 2023"
-        side="left"
-        icon="done_all"
-      >
-        <div>Erster Aufbau der Website wurde begonnen und die ersten Seiten wurden erstellt.</div>
-      </q-timeline-entry>
-
-      <q-timeline-entry
-        title="Fertigstellung der Website"
-        subtitle="6 Jänner 2024"
-        side="right"
-        icon="done_all"
-      >
-        <div>Website wurde fertiggestellt und alle Seiten wurden erstellt.</div>
-      </q-timeline-entry>
-
-      <q-timeline-entry title="Präsentation" subtitle="9 Jänner 2024" side="left" icon="done_all">
-        <div>
-          Die Website wurde präsentiert und die Dokumentation wurde abgegeben. Das Projekt wurde
-          somit beendet.
-        </div>
-      </q-timeline-entry>
-
       <!-- Nachtkasten Part der Roadmap -->
 
       <q-timeline-entry heading>Nachtkastel</q-timeline-entry>
@@ -106,6 +67,45 @@
       <q-timeline-entry title="Präsentation" subtitle="9 Jänner 2024" side="left" icon="done_all">
         <div>
           Das Nachtkastel wurde präsentiert und die Dokumentation wurde abgegeben. Das Projekt wurde
+          somit beendet.
+        </div>
+      </q-timeline-entry>
+
+      <!-- Website Part der Roadmap -->
+
+      <q-timeline-entry heading>Website</q-timeline-entry>
+
+      <q-timeline-entry title="Wireframe" subtitle="24 November 2023" side="left" icon="done_all">
+        <div>Wireframe wurde erstellt und die Struktur der Website wurde festgelegt.</div>
+      </q-timeline-entry>
+
+      <q-timeline-entry title="Mockup" subtitle="27 November 2023" side="right" icon="done_all">
+        <div>
+          Mockup wurde erstellt und die Farben und das Design der Website wurden festgelegt.
+        </div>
+      </q-timeline-entry>
+
+      <q-timeline-entry
+        title="Beginn der Website"
+        subtitle="1 Dezember 2023"
+        side="left"
+        icon="done_all"
+      >
+        <div>Erster Aufbau der Website wurde begonnen und die ersten Seiten wurden erstellt.</div>
+      </q-timeline-entry>
+
+      <q-timeline-entry
+        title="Fertigstellung der Website"
+        subtitle="6 Jänner 2024"
+        side="right"
+        icon="done_all"
+      >
+        <div>Website wurde fertiggestellt und alle Seiten wurden erstellt.</div>
+      </q-timeline-entry>
+
+      <q-timeline-entry title="Präsentation" subtitle="9 Jänner 2024" side="left" icon="done_all">
+        <div>
+          Die Website wurde präsentiert und die Dokumentation wurde abgegeben. Das Projekt wurde
           somit beendet.
         </div>
       </q-timeline-entry>
